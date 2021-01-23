c127d0aa872527db410d6ae7644a9d99f0a35d9b
Dan <dan.mok999@yandex.ru>
2021-01-23 22:48:07 +0500

    Refactored BrsUrlProvider

--- src/apis/brsUrlProvider.ts
+++ src/apis/brsUrlProvider.ts
@@ -1,15 +1,14 @@
+const brsUrl = 'https://brs.urfu.ru/mrd';
+const corsProxy = 'https://kamikoto-cors-proxy.herokuapp.com';
+
 export default class BrsUrlProvider {
     private readonly withProxy: boolean;
-    private readonly brsUrl = 'https://brs.urfu.ru/mrd';
-    private readonly corsProxy = 'https://kamikoto-cors-proxy.herokuapp.com';
 
     constructor(withProxy: boolean) {
         this.withProxy = withProxy;
     }
 
     get baseUrl() {
-        return this.withProxy ?
-            `${this.corsProxy}/${this.brsUrl}` :
-            this.brsUrl;
+        return this.withProxy ? `${corsProxy}/${brsUrl}` : brsUrl;
     }
 }
