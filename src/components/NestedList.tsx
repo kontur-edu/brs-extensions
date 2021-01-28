3040bbfed0964f74a645606cec60d76d3d567014
Dan <dan.mok999@yandex.ru>
2021-01-28 18:53:04 +0500

    Refactor: functions separation

--- src/components/NestedList.tsx
+++ src/components/NestedList.tsx
@@ -26,60 +26,25 @@ const useStyles = makeStyles((theme: Theme) =>
     }),
 );
 
-function NestedListItem(props: NestedListItemProps) {
-    const {item, collapsed} = props;
-    const [open, setOpen] = React.useState(!collapsed);
-    const classes = useStyles();
-
-    return (
-        <React.Fragment>
-            <ListItem button onClick={() => setOpen(!open)}>
-                <ListItemIcon>
-                    <ViewModuleIcon/>
-                </ListItemIcon>
-                <ListItemText primary={item.title}/>
-                {item.nestedItems?.length && (open ? <ExpandLess/> : <ExpandMore/>) || undefined}
-            </ListItem>
-            {
-                item.nestedItems?.length &&
-                item.nestedItems.map((nestedItemTitle, index) => (
-                    <Collapse key={index} in={open} timeout="auto" unmountOnExit>
-                        <List component="div" className={classes.nested} disablePadding>
-                            <ListItem button>
-                                <ListItemIcon>
-                                    <GroupIcon/>
-                                </ListItemIcon>
-                                <ListItemText primary={nestedItemTitle}/>
-                            </ListItem>
-                        </List>
-                    </Collapse>
-                ))
-                || undefined
-            }
-        </React.Fragment>
-    );
-}
-
 export default function NestedList(props: NestedListProps) {
     const {title, items, collapsed = true} = props;
     const classes = useStyles();
+
+    const listSubheader = (
+        <ListSubheader component="div" id="nested-list-subheader" hidden={!title}>
+            {title}
+        </ListSubheader>
+    );
+
     return (
         <List
             component="nav"
             aria-labelledby="nested-list-subheader"
-            subheader={
-                <ListSubheader component="div" id="nested-list-subheader" hidden={!title}>
-                    {title}
-                </ListSubheader>
-            }
+            subheader={listSubheader}
             className={classes.root}>
             {
                 items.length ?
-                    items.map((item, index) => (
-                        <NestedListItem key={index}
-                                        item={item}
-                                        collapsed={collapsed}/>
-                    )) :
+                    ConstructItems(items, collapsed) :
                     <ListItem className={classes.onEmptyMessage}>
                         <ListItemText primary="No items"/>
                     </ListItem>
@@ -88,18 +53,76 @@ export default function NestedList(props: NestedListProps) {
     );
 }
 
+function ConstructItems(items: NestedListItem[], collapsed: boolean) {
+    return items.map((item, index) => (
+        <Item key={index}
+              item={item}
+              collapsed={collapsed}/>
+    ));
+}
+
+function Item(props: ItemProps) {
+    const {item, collapsed} = props;
+    const [open, setOpen] = React.useState(!collapsed);
+    const {title, nestedItems} = item;
+
+    const hasSubItems = nestedItems && nestedItems.length > 0; //(item.nestedItems ?? []).length > 0;
+
+    return (
+        <React.Fragment>
+            <ListItem button onClick={() => setOpen(!open)}>
+                <ListItemIcon>
+                    <ViewModuleIcon/>
+                </ListItemIcon>
+                <ListItemText primary={title}/>
+                {hasSubItems && (open ? <ExpandLess/> : <ExpandMore/>)}
+            </ListItem>
+            {
+                nestedItems?.map((nestedItemTitle, index) =>
+                    <NestedItem {...{index, open, nestedItemTitle}}/>)
+            }
+        </React.Fragment>
+    );
+}
+
+function NestedItem({index, nestedItemTitle, open}: NestedItemProps) {
+    const classes = useStyles();
+
+    return (
+        <Collapse key={index} in={open} timeout="auto" unmountOnExit>
+            <List component="div" className={classes.nested} disablePadding>
+                <ListItem button>
+                    <ListItemIcon>
+                        <GroupIcon/>
+                    </ListItemIcon>
+                    <ListItemText primary={nestedItemTitle}/>
+                </ListItem>
+            </List>
+        </Collapse>
+    );
+}
+
 export interface NestedListItem {
     title: string,
     nestedItems?: string[]
 }
 
-interface NestedListProps {
-    title?: string;
+interface ItemsProps {
     items: NestedListItem[];
     collapsed?: boolean;
 }
 
-interface NestedListItemProps {
-    item: { title: string, nestedItems?: string[] };
+interface NestedListProps extends ItemsProps {
+    title?: string;
+}
+
+interface ItemProps {
+    item: NestedListItem;
     collapsed?: boolean;
+}
+
+interface NestedItemProps {
+    index: number;
+    nestedItemTitle: string;
+    open: boolean;
 }
