ae2e37c02e1e567c306c20f174f0345215205267
Dan <dan.mok999@yandex.ru>
2021-01-30 18:07:46 +0500

    Refactored googleAuth

--- src/apis/googleAuth.ts
+++ src/apis/googleAuth.ts
@@ -2,33 +2,24 @@ const CLIENT_ID = '122993083593-pacve8csj86voko30ia65raeg0ncrtuv.apps.googleuser
 const DISCOVERY_DOCS = ["https://sheets.googleapis.com/$discovery/rest?version=v4"];
 const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
 
-let onAuthStatusChangedEvent: (isSignedIn: boolean) => void;
-let onErrorEvent: (error: any) => void;
-
 const googleAuth = {
     async init() {
         // @ts-ignore
         if (gapi.client)
             return;
-        await new Promise(res => {
+        return new Promise(resolve => {
             gapi.load('client:auth2', async () => {
                 await gapi.client.init({
                     clientId: CLIENT_ID,
                     discoveryDocs: DISCOVERY_DOCS,
                     scope: SCOPES
-                }).then(() => {
-                    if (!onAuthStatusChangedEvent)
-                        return;
-                    // @ts-ignore
-                    const signedIn = gapi.auth2.getAuthInstance().isSignedIn;
-                    signedIn.listen(onAuthStatusChangedEvent);
-                }, onErrorEvent);
-                res();
+                }).catch(console.error);
+                resolve();
             });
         });
     },
 
-    checkAuth() {
+    checkAuthorized() {
         // @ts-ignore
         return gapi.auth2?.getAuthInstance()?.isSignedIn?.get();
     },
