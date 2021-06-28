37e132b22b78a1ba0f52cf4243b0f685d87d2662
Домашних Иван <digi@skbkontur.ru>
2021-06-28 12:27:55 +0500

    Continue after bad group techcard

--- src/managers/MarksManager.ts
+++ src/managers/MarksManager.ts
@@ -66,7 +66,6 @@ export default class MarksManager {
           this.reportManager.finishReport();
         } else {
           this.reportManager.cancelReport();
-          break;
         }
 
         if (this.cancelPending) {
