f6a1af562f0b84c09ebaa42d3c7c1ebeac6cc34c
Домашних Иван <digi@skbkontur.ru>
2022-02-03 00:15:25 +0500

    Starting local start

---
+++ src/apis/googleLocalApi.ts
@@ -0,0 +1,89 @@
+import { google } from "googleapis";
+import { OAuth2Client } from "googleapis-common";
+import * as googleAuth from "./googleLocalAuth";
+
+let globalAuth: OAuth2Client | null = null;
+
+export async function authorizeAsync(policy: AuthorizePolicy) {
+  globalAuth = await googleAuth.authorizeAsync(policy);
+}
+
+export function openSpreadsheet(spreadsheetId: string): Spreadsheet {
+  const auth = getGlobalAuth();
+
+  async function readAsync(range: string) {
+    const sheets = google.sheets({ version: "v4", auth });
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId,
+      range,
+    });
+    return response.data;
+  }
+
+  function writeAsync(range: string, values: any[][], asEnteredByUser = false) {
+    const sheets = google.sheets({ version: "v4", auth });
+    const valueInputOption = asEnteredByUser ? "USER_ENTERED" : "RAW";
+    const requestBody = {
+      values,
+    };
+    return sheets.spreadsheets.values.update({
+      spreadsheetId,
+      range,
+      valueInputOption,
+      requestBody,
+    });
+  }
+
+  function appendAsync(
+    range: string,
+    values: any[][],
+    asEnteredByUser = false
+  ) {
+    const sheets = google.sheets({ version: "v4", auth });
+    const valueInputOption = asEnteredByUser ? "USER_ENTERED" : "RAW";
+    const requestBody = {
+      values,
+    };
+    return sheets.spreadsheets.values.append({
+      spreadsheetId,
+      range,
+      valueInputOption,
+      requestBody,
+    });
+  }
+
+  return {
+    readAsync,
+    writeAsync,
+    appendAsync,
+  };
+}
+
+function getGlobalAuth() {
+  if (!globalAuth) {
+    throw new Error("Not authenticated. Use authorizeAsync to authenticate");
+  }
+  return globalAuth;
+}
+
+export type AuthorizePolicy = googleAuth.AuthorizePolicy;
+
+export interface Spreadsheet {
+  readAsync: (range: string) => Promise<ValueRange>;
+  writeAsync: (
+    range: string,
+    values: any[][],
+    asEnteredByUser?: boolean
+  ) => Promise<any>;
+  appendAsync: (
+    range: string,
+    values: any[][],
+    asEnteredByUser?: boolean
+  ) => Promise<any>;
+}
+
+export interface ValueRange {
+  majorDimension?: string | null;
+  range?: string | null;
+  values?: any[][] | null | undefined;
+}
