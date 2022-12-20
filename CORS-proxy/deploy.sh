b1ed65290305ed394291bc9b0b04b7ddc19a4da7
Домашних Иван <digi@skbkontur.ru>
2022-12-20 23:27:00 +0500

    ycloud serverless proxy deploy

---
+++ CORS-proxy/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export CONTAINER_REPOSITORY_NAME=cr.yandex/crprkhjar1gq4t8h8u0t/brs-extensions-proxy:latest
+export SERVERLESS_CONTAINER_ID=bbamc5stve2edro7t455
+export FOLDER_ID=b1gupi6tnkioua1397gl
+export SA_ID=aje2oovtau2cioluf9tp
+
+
+docker build -t ${CONTAINER_REPOSITORY_NAME}:latest . ;
+docker push ${CONTAINER_REPOSITORY_NAME}:latest;
+
+yc sls container revisions deploy \
+	--folder-id ${FOLDER_ID} \
+	--container-id ${SERVERLESS_CONTAINER_ID} \
+	--memory 256M \
+	--cores 1 \
+	--execution-timeout 30s \
+	--concurrency 8 \
+	--service-account-id ${SA_ID} \
+	--image "${CONTAINER_REPOSITORY_NAME}:latest";
